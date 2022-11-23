22f67ccb6fdb754025b8ea44a306e9b6122ba0ce
Nithesh0812 <mudsunithesh0812@gmail.com>
2022-11-23 11:59:26 +0530

    file pushed

---
+++ src/main/java/com/app/demo/Exam/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab Internal 1 exam.The created project is exam" );
+    }
+}
